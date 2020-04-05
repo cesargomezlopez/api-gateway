b1f6f4a571d728877e32f46344060b12b642db0b
Cesar Gomez <cesar.gomez@urp.edu.pe>
2020-04-04 23:16:07 -0500

    Project Created

---
+++ src/main/java/bootcamp/everis/gateway/config/BeanConfig.java
@@ -0,0 +1,38 @@
+package bootcamp.everis.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableEurekaClient
+@EnableHystrix
+@Configuration
+public class BeanConfig {
+
+  @Bean
+  public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+    return builder.routes()
+            .route(r -> r.path("/client/**")
+                    .filters(f -> f.hystrix(c -> c.setName("client")
+                            .setFallbackUri("forward:/fallback/message")))
+                    .uri("lb://MS-CLIENT/")
+                    .id("ms-client"))
+
+            .route(r -> r.path("/bankAccount/**")
+                    .filters(f -> f.hystrix(c -> c.setName("bankAccount")
+                            .setFallbackUri("forward:/fallback/message")))
+                    .uri("lb://MS-BANK-ACCOUNT/")
+                    .id("ms-bank-account"))
+
+            .route(r -> r.path("/creditAccount/**")
+                    .filters(f -> f.hystrix(c -> c.setName("creditAccount")
+                            .setFallbackUri("forward:/fallback/message")))
+                    .uri("lb://MS-CREDIT-ACCOUNT/")
+                    .id("ms-credit-account"))
+            .build();
+  }
+
+}
